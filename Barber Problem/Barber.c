e98c4452cb3027385ddd01fa0f6a7471e0e122a9
Jainam Doshi <s3825891@student.rmit.edu.au>
2021-09-05 16:33:38 +0400

    Adding queue

--- Barber Problem/Barber.c
+++ Barber Problem/Barber.c
@@ -8,14 +8,19 @@
 
 #define SLOT_SIZE 5
 #define BARBER_THREADS 1
-#define PROGRAM_TIME_IN_SEC 2
+#define PROGRAM_TIME_IN_SEC 10
 
 pthread_t slot[SLOT_SIZE];
 pthread_mutex_t mutex;
 pthread_cond_t notifyBarber;
 
 int emptySeat[SLOT_SIZE];
-int emptySeatIndex = 0;
+int emptySeatStart = 0;
+int emptySeatEnd = 0;
+
+int occupiedSeat[SLOT_SIZE];
+int occupiedSeatStart = 0;
+int occupiedSeatEnd = 0;
 
 int status = 1;
 int numCustomer = 0;
@@ -29,22 +34,23 @@ void* barber(void* args) {
 
 
         pthread_mutex_lock(&mutex);
-        if (numCustomer == 0) {
+        if (occupiedSeat[occupiedSeatStart] == -1) {
             printf("Barber (ID: %lu) is sleeping\n", id);
             pthread_cond_wait(&notifyBarber, &mutex);
             printf("Barber (ID: %lu) is waking up\n", id);
         }
 
+        printf("Barber (ID: %lu) serving customer on slot %d\n", id, occupiedSeat[occupiedSeatStart]);
+        sleep((double)(rand() % 1000) / 1000);
+        slot[occupiedSeat[occupiedSeatStart]] = 0;
 
-        for (int i = 0; i < SLOT_SIZE && numCustomer != 0; i++) {
 
-            if (slot[i] != 0) {
-                printf("Barber (ID: %lu) serving customer on slot %d\n", id, i);
-                sleep(0.5);
-                slot[i] = 0;
-                numCustomer--;
-            }
-        }
+        emptySeat[emptySeatEnd] = occupiedSeat[occupiedSeatStart];
+        emptySeatEnd = (emptySeatEnd + 1) % SLOT_SIZE;
+
+        occupiedSeat[occupiedSeatStart] = -1;
+        occupiedSeatStart = (occupiedSeatStart + 1) % SLOT_SIZE;
+
         pthread_mutex_unlock(&mutex);
     }
 
@@ -58,31 +64,35 @@ void* customer(void* args) {
     // if (numCustomer < SLOT_SIZE) {
 
     pthread_mutex_lock(&mutex);
-    if (slot[emptySeatIndex] == 0) {
-        slot[i] = id;
-        printf("Customer (ID: %lu) has arrived the barber shop and is sitting on seat %d\n", id, i);
-        numCustomer++;
+    if (emptySeat[emptySeatStart] != -1) {
+        slot[emptySeat[emptySeatStart]] = id;
+        printf("Customer (ID: %lu) has arrived the barber shop and is sitting on seat %d\n", id, emptySeat[emptySeatStart]);
+
+
+        occupiedSeat[occupiedSeatEnd] = emptySeat[emptySeatStart];
+        occupiedSeatEnd = (occupiedSeatEnd + 1) % SLOT_SIZE;
+        emptySeat[emptySeatStart] = -1;
+        emptySeatStart = (emptySeatStart + 1) % SLOT_SIZE;
+
+    } else {
+        printf("Customer (ID: %lu) left because no space available\n", id);
     }
     pthread_mutex_unlock(&mutex);
     pthread_cond_signal(&notifyBarber);
-    // }
 
-} else {
-    printf("Customer (ID: %lu) left because no space available\n", id);
+    return EXIT_SUCCESS;
 }
-return EXIT_SUCCESS;
-}
+
 
 
 void* customerGenerator(void* args) {
 
     while (status) {
 
-        sleep(1);
+        sleep((double)(rand() % 1000) / 1000);
         pthread_t customerThread;
         pthread_create(&customerThread, NULL, &customer, NULL);
-        printf("---------\n");
-        pthread_join(customerThread, NULL);
+        // pthread_join(customerThread, NULL);
 
     }
 
@@ -95,7 +105,14 @@ int main(void) {
 
     for (int i = 0; i < SLOT_SIZE; i++) {
         emptySeat[i] = i;
+        occupiedSeat[i] = -1;
     }
+
+
+    // for (int i = 0; i < SLOT_SIZE; i++) {
+    //     printf("%d\n", occupiedSeat[i]);
+    // }
+
 
     pthread_t barberThreads[BARBER_THREADS];
     pthread_mutex_init(&mutex, NULL);
